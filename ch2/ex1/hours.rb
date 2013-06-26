c43a00c1d0c47999c8325578a352befa4b7a31f6
ckmarcelus1 <ckmarcelus@gmail.com>
2013-06-26 10:48:44 -0500

    "updates with hours exercises"
    "updates with hours exercises"
    
    git reset HEAD <file>

--- ch2/ex1/hours.rb
+++ ch2/ex1/hours.rb
@@ -1,1 +1,17 @@
 puts "Im going to calculate the number of hours in the year.."
+	h = (365 * 24)  #hours in a yeah
+	m = (h * 60)	#minutes in a year
+	s = (m * 60)	#seconds in a year
+	 puts h 
+
+puts "Im going to calculate the number of decades in a year"
+	d = (m * 10)
+		puts d
+
+puts "I'm going to calculate my age in seconds"
+	c = (s * 24)
+		puts c
+
+puts "The author's age is "
+	a = (1111000000 / s) 
+		puts a
