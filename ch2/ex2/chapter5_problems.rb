f1c6b2821705060f5fec4823ae3a670ac9159d6b
ckmarcelus1 <ckmarcelus@gmail.com>
2013-06-27 18:00:47 -0500

    chapter 5 first problem

---
+++ ch2/ex2/chapter5_problems.rb
@@ -0,0 +1,9 @@
+puts  "Hi, what's your name? "
+name1 = gets.chomp
+puts "Nice, " + name1 + " Whats your middle name? "
+name2 = gets.chomp
+name3 = name1  + " " + name2
+puts "Cool, " + name3 + " Finally, What's your last name?"
+name4 = gets.chomp
+name5 = name3  + " " + name4
+puts "Great meeting you " + name5 + "!"
