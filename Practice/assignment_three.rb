222ddac9b3a329ce6171d6814e97967f2f8b3b71
ckmarcelus1 <ckmarcelus@gmail.com>
2013-07-02 15:11:15 -0500

    some more cleaning

---
+++ Practice/assignment_three.rb
@@ -0,0 +1,18 @@
+n = 99
+m = 70
+
+while n >= m
+	puts "There are " + n.to_s + " beers on the wall!"
+	n = n-1
+
+		puts "Drink one down throw it around and now there's " + n.to_s + " beers on the wall" 		
+	end
+
+puts "Now there's no more beers on the wall"	
+puts "AND NOW WE'RE WASTED!!"
+
+ 	
+
+	
+
+
---
+++ Practice/seven.rb
@@ -0,0 +1,7 @@
+puts "Hello whats your name?"
+name = gets.chomp
+puts "Hello, " + name + "."
+
+if name == "Chris" || name == "Katey" # "||" is for "or"
+ 	puts "That's a great name!"	#"&&" is for "and"
+end		# "!" is for not
