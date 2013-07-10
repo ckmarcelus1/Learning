cb27706c2fd90b31ee67efb6639e58ee7c8a8193
ckmarcelus1 <ckmarcelus@gmail.com>
2013-07-10 16:35:25 -0500

    assignment three is done

--- Practice/assignment_three.rb
+++ Practice/assignment_three.rb
@@ -1,18 +1,43 @@
 n = 99
-m = 70
+m = 90
 
 while n >= m
-	puts "There are " + n.to_s + " beers on the wall!"
+	puts "There are " + n.to_s + " bottles of beer on the wall," + n.to_s + " bottles of beer on the wall"
 	n = n-1
 
-		puts "Drink one down throw it around and now there's " + n.to_s + " beers on the wall" 		
+		puts "Drink one down throw it around and now there's " + n.to_s + "5 bottles of beers on the wall" 		
 	end
 
 puts "Now there's no more beers on the wall"	
-puts "AND NOW WE'RE WASTED!!"
 
- 	
 
+
+
+
+while true
+ 	puts "Hhuhh?!? speak up"
+	word = gets.chomp
+
+	if word == "BYE"
+		puts "BYE SWEETIE!"
+		break
+
+		
+	end
+	if word != word.upcase
+		puts "HUH? SPEAK UP, SONNY!!"
+	else
+		year = 1930 +rand(21)
+		puts "No, not since " + year.to_s+"!"
+		break
+		
+	
+	end
+	
+	
+	
+
+end
 	
 
 
