100887b30e298e41aff1322d8d798c16de9eed4c
ckmarcelus1 <ckmarcelus@gmail.com>
2013-07-19 13:38:51 -0500

    I just realligned my folders

--- Practice/three.rb
+++
@@ -1,5 +0,0 @@
-puts "you're swell"
-
-puts 2 * 5 #shows how numbers interact
-puts "2" * 5 #shows how strings and numbers interact in that you can multiply strings
-puts "2 * 5" #shows how important strings are
