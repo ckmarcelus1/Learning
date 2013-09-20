ae2bb471438524ab5a35f306ab25a020b874d22d
ckmarcelus1 <ckmarcelus@gmail.com>
2013-09-20 12:53:08 -0500

    26 & 29

---
+++ Assignments/thehardway/ex29.rb
@@ -0,0 +1,30 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+	puts "too many cats!"
+end
+
+if people > cats
+	puts "not too many cats!"
+end
+
+if people < dogs
+	puts "the word is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+	puts "People are greater than or equal to dogs"
+end
+
+if people <= dogs
+	puts "people are less than or equal"
+end
+
+if people == dogs
+	puts "people are dogs.."
+end
+
