245669b611c96d8581a88bf7217d284b0295c69a
ckmarcelus1 <ckmarcelus@gmail.com>
2013-12-10 12:54:50 -0600

    added some stuff

--- railscourse/32.rb
+++ railscourse/32.rb
@@ -1,9 +1,36 @@
 require 'active_record'
 
-class Order < active_record::Base
+class Order < active_record::Base # Active record is  great in that its an ORM that works best with Ruby
 	end
 
 order = Order.find(1)
 order.pay_type = "Purchase Order"
 order.save
+
+puts
+puts
+
+
+a = ["ant", "bee", "cat", "dog"]
+#same as 
+a %w{ant bee cat dog elk}
+
+inst_section = {
+:cello => 'string',
+:clarinet => 'woodwind',
+:drum => 'percussion',
+:oboe => 'woodwind',
+:trumpet => 'brass',
+:violin => 'string'
+}
+#the same as
+inst_section = {
+cello: 'string',
+clarinet: 'woodwind',
+drum: 'percussion',
+oboe: 'woodwind',
+trumpet: 'brass',
+violin: 'string'
+}
+
 
